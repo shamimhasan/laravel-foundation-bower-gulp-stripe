3ddb41b017a566b0fa8a322f20d19fb662547a1a
Shamim <shamimhasan@ovi.com>
2014-10-25 10:26:45 +0600

    db update

--- db/db.sql
+++ db/db.sql
@@ -9,7 +9,45 @@
 /*!40101 SET NAMES utf8 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Dumping database structure for stripelaravel
+DROP DATABASE IF EXISTS `stripelaravel`;
+CREATE DATABASE IF NOT EXISTS `stripelaravel` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `stripelaravel`;
+
+
+-- Dumping structure for table stripelaravel.customers
+DROP TABLE IF EXISTS `customers`;
+CREATE TABLE IF NOT EXISTS `customers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `stripe_user_id` varchar(50) NOT NULL,
+  `access_token` varchar(50) NOT NULL,
+  `refresh_token` varchar(50) NOT NULL,
+  `livemode` varchar(50) NOT NULL,
+  `stripe_publishable_key` varchar(50) NOT NULL,
+  `token_type` varchar(50) NOT NULL,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 -- Data exporting was unselected.
+
+
+-- Dumping structure for table stripelaravel.users
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `customer_id` int(11) DEFAULT NULL,
+  `user_id` varchar(500) NOT NULL DEFAULT '0',
+  `description` varchar(500) DEFAULT NULL,
+  `email` varchar(500) DEFAULT NULL,
+  `cards` varchar(500) DEFAULT NULL,
+  `default_card` varchar(500) DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Data exporting was unselected.
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
